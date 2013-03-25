4d1c5933592eb7ea219205f055e7e36fcc1fbf0a
Vincent Paca <vincent.paca@gmail.com>
2013-03-26 07:54:32 +0800

    Fixed token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,5 +2,5 @@ secret = ENV['GABBLE_SECRET']
 if secret.length < 30
   raise "Secret token cannot be loaded"
 else
-  Gabble::Application.config.secret_token =  secret
+  GabblePages::Application.config.secret_token =  secret
 end
